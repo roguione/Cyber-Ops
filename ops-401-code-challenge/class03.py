e1c8e28fc08fa043c8933810eaba2466eafa5391
roguione <myjobcontacts@yahoo.com>
2023-04-19 23:05:46 -0500

    last mod works right

--- ops-401-code-challenge/class03.py
+++ ops-401-code-challenge/class03.py
@@ -4,69 +4,6 @@
 # Uptime Sensor
 # 04/19/23
 
-import smtplib
-import datetime
-import subprocess
-import time
-from getpass import getpass
-
-# Ask user for email and password to use for sending notifications
-email = input("Enter your email address: ")
-password = getpass("Enter your password: ")
-
-# Set up SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-
-# Log in to email account
-s.login(email, password)
-
-# Initialize host status
-host_up = True
-
-# Function to send email alert
-def send_alert(status_before, status_after):
-    # Get current timestamp
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-    # Set email message
-    if status_before == "up" and status_after == "down":
-        subject = "Host Down Alert"
-        message = f"The host is down! Host status changed from up to down at {now}."
-    elif status_before == "down" and status_after == "up":
-        subject = "Host Up Alert"
-        message = f"The host is up! Host status changed from down to up at {now}."
-    else:
-        return # No status change, so no alert needed
-    
-    # Send email
-    s.sendmail(email, email, f"Subject: {subject}\n\n{message}")
-
-# Function to check host status and send email alert if status changes
-def check_host():
-    # Ping host
-    result = subprocess.run(['ping', '-c', '1', 'google.com'], stdout=subprocess.PIPE)
-    
-    # Check if host is up or down
-    if "1 received" in result.stdout.decode('utf-8'):
-        current_status = "up"
-    else:
-        current_status = "down"
-    
-    # Check if status has changed
-    global host_up
-    if current_status != host_up:
-        send_alert("up" if host_up else "down", current_status)
-        host_up = current_status
-
-# Run check_host function in a loop
-while True:
-    check_host()
-    # Wait 1 minute before checking again
-    time.sleep(60)
-
-# Close SMTP session
-s.quit()
 import os
 import datetime
 import time
@@ -74,64 +11,73 @@ import smtplib
 import subprocess
 from getpass import getpass
 
+
+# Get target IP address from user
+target_ip = input("Enter the target IP address: ")
+
 # Ask user for email and password to use for sending notifications
 email = input("Enter your email address: ")
 password = getpass("Enter your password: ")
 
 # Set up SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
+smtp_server = "smtp.gmail.com"
+smtp_port = 587
+smtp_session = smtplib.SMTP(smtp_server, smtp_port)
+smtp_session.starttls()
 
 # Log in to email account
-s.login(email, password)
-
-# Get target IP address from user
-target_ip = input("Enter the target IP address: ")
+smtp_session.login(email, password)
 
 # Initialize host status
 host_up = True
 
 # Function to ping target address and print status and timestamp
 def ping_target(target):
-    # Receive a single ICMP packet
-    icmp = os.system("ping -c 1 " + target)
+    # Ping the target
+    icmp = os.system(f"ping -c 1 {target}")
+    
+    # Determine whether the ping was successful
     if icmp == 0:
         status = "up"
     else:
         status = "down"
+    
     # Get current timestamp
-    currentTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_time = datetime.datetime.now()
+    
     # Print status, timestamp, and destination IP tested
-    print(f"{currentTime} Network {status} to {target}")
+    print(f"{current_time} Network {status} to {target}")
+    
     # Return the status
     return status
+
+# Function to send email alert
+def send_alert(status_before, status_after):
+    # Get current timestamp
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    # Set email subject and message
+    if status_before == "up" and status_after == "down":
+        subject = "Host Down Alert"
+        message = f"The host is down! Host status changed from up to down at {current_time}."
+    elif status_before == "down" and status_after == "up":
+        subject = "Host Up Alert"
+        message = f"The host is up! Host status changed from down to up at {current_time}."
+    else:
+        return # No status change, so no alert needed
+    
+    # Send email
+    smtp_session.sendmail(email, email, f"Subject: {subject}\n\n{message}")
 
 # Set initial success and failure counters to 0
 success = 0
 failure = 0
 
-# Function to send email alert
-def send_alert(status_before, status_after):
-    # Get current timestamp
-    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    
-    # Set email message
-    if status_before == "up" and status_after == "down":
-        subject = "Host Down Alert"
-        message = f"The host is down! Host status changed from up to down at {now}."
-    elif status_before == "down" and status_after == "up":
-        subject = "Host Up Alert"
-        message = f"The host is up! Host status changed from down to up at {now}."
-    else:
-        return # No status change, so no alert needed
-    
-    # Send email
-    s.sendmail(email, email, f"Subject: {subject}\n\n{message}")
-
 # Continuous ping loop
 while True:
-    # Call ping_target function to ping the target and print status, timestamp, and destination IP
+    # Ping the target and print status, timestamp, and destination IP
     status = ping_target(target_ip)
+    
     # Increment success or failure counters
     if status == "up":
         success += 1
@@ -139,22 +85,26 @@ while True:
     else:
         success = 0
         failure += 1
+    
     # Check if there have been 3 consecutive failures or successes
     if success == 3:
+        print(f"{target_ip} is up!")
         if not host_up:
             send_alert("down", "up")
             host_up = True
         success = 0
         failure = 0
     elif failure == 3:
+        print(f"{target_ip} is down!")
         if host_up:
             send_alert("up", "down")
             host_up = False
         success = 0
         failure = 0
+    
     # Wait for 2 seconds before sending the next packet
     time.sleep(2)
 
 # Close SMTP session
-s.quit()
+smtp_session.quit()
 
