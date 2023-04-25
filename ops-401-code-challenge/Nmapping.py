6365ea4ba33b4868ec42bf20894f36b3644aca83
roguione <myjobcontacts@yahoo.com>
2023-04-25 13:15:23 -0500

    tcp syn time adj

--- ops-401-code-challenge/Nmapping.py
+++ ops-401-code-challenge/Nmapping.py
@@ -11,8 +11,14 @@
 import nmap
 import os
 
-# Define target subnet to scan
-subnet = input("Please enter the IP address or subnet to scan (e.g. 192.168.1.0/24): ")
+# Get target IP address from user input
+subnet = input("Enter target IP address (with CIDR notation): ")
+
+# Get password from user input
+password = input("Enter password for sudo access: ")
+
+# Get TCP SYN scan time from user input
+tcp_syn_scan_time = input("Enter TCP SYN scan time (in seconds): ")
 
 # Create Nmap scanner object
 nm = nmap.PortScanner()
@@ -23,15 +29,12 @@ nm.scan(hosts=subnet, arguments='-sn')
 # Create a list to store vulnerability scan results
 results = []
 
-# Prompt user for password to run the vulnerability scan
-password = input("Please enter your password to run the vulnerability scan: ")
-
 # Loop through identified live hosts and run Nmap vulnerability scan
 for host in nm.all_hosts():
     if nm[host]['status']['state'] == 'up':
         print(f"Scanning {host} for vulnerabilities...")
         # Run Nmap vulnerability scan on live host with sudo
-        output = os.popen(f"echo {password} | sudo -S nmap -sS -sV -T2 -p- -R --randomize-hosts --min-rate=20 {host} --script vuln").read()
+        output = os.popen(f"echo {password} | sudo -S nmap -sS -sV -T2 -p- -R --randomize-hosts --min-rate=20 --max-retries 1 --max-scan-delay 0 --max-rtt-timeout 150ms --initial-rtt-timeout {tcp_syn_scan_time}s {host} --script vuln").read()
         # Append vulnerability scan results to list
         results.append(f"Results for {host}:\n{output}\n")
 
