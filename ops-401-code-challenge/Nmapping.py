ee0a732116ea5483dc57e5a472cc036bb283fb58
roguione <myjobcontacts@yahoo.com>
2023-04-25 12:42:29 -0500

    elevated priv mod

--- ops-401-code-challenge/Nmapping.py
+++ ops-401-code-challenge/Nmapping.py
@@ -5,14 +5,14 @@
 # Saves results to desktop
 # Note that while using Nmap stealth scan, it may not completely evade detection
 # Therefore, it is important to have appropriate authorization
-# Eollow the legal and ethical guidelines before performing any network scans
+# Follow the legal and ethical guidelines before performing any network scans
 # Especially if your network is monitored for suspicious activities
 
 import nmap
 import os
 
 # Define target subnet to scan
-subnet = "192.168.1.115/24"
+subnet = input("Please enter the IP address or subnet to scan (e.g. 192.168.1.0/24): ")
 
 # Create Nmap scanner object
 nm = nmap.PortScanner()
@@ -23,12 +23,15 @@ nm.scan(hosts=subnet, arguments='-sn')
 # Create a list to store vulnerability scan results
 results = []
 
+# Prompt user for password to run the vulnerability scan
+password = input("Please enter your password to run the vulnerability scan: ")
+
 # Loop through identified live hosts and run Nmap vulnerability scan
 for host in nm.all_hosts():
     if nm[host]['status']['state'] == 'up':
         print(f"Scanning {host} for vulnerabilities...")
-        # Run Nmap vulnerability scan on live host
-        output = os.popen(f"sudo nmap -sS -sV -T2 -p- -R --randomize-hosts --min-rate=20 {host} --script vuln").read()
+        # Run Nmap vulnerability scan on live host with sudo
+        output = os.popen(f"echo {password} | sudo -S nmap -sS -sV -T2 -p- -R --randomize-hosts --min-rate=20 {host} --script vuln").read()
         # Append vulnerability scan results to list
         results.append(f"Results for {host}:\n{output}\n")
 
@@ -36,5 +39,3 @@ for host in nm.all_hosts():
 with open(os.path.join(os.path.expanduser("~"), "Desktop", "vulnerability_scan_results.txt"), "w") as f:
     f.writelines(results)
     print("Results saved to vulnerability_scan_results.txt on the desktop.")
-
-
