0d6adbe166c66b43e37db8e8ab7af15d97b63800
roguione <myjobcontacts@yahoo.com>
2023-04-25 13:42:12 -0500

    raw mod 2

--- ops-401-code-challenge/Nmapping.py
+++ ops-401-code-challenge/Nmapping.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-# Nmap script to scan for live hosts and open ports on a network and save results to a text file
+# Nmap mapping/vulnerability Stealth scan script
+# Is called when uptime sensor declares UP for 5 mins
+# Saves results to desktop
 # Note that while using Nmap stealth scan, it may not completely evade detection
 # Therefore, it is important to have appropriate authorization
 # Follow the legal and ethical guidelines before performing any network scans
@@ -16,22 +18,26 @@ subnet = input("Enter the subnet to scan (e.g., 192.168.1.0/24): ")
 nm = nmap.PortScanner()
 
 # Use Nmap to scan target subnet and identify live hosts and open ports
-nm.scan(hosts=subnet, arguments='-p- -sT')
+nm.scan(hosts=subnet, arguments='-p- -sT -T4')
 
-# Create a list to store scan results
+# Create a list to store scan and vulnerability scan results
 results = []
 
-# Loop through identified live hosts and append results to list
+# Loop through identified live hosts and run quick Nmap vulnerability scan
 for host in nm.all_hosts():
     if nm[host]['status']['state'] == 'up':
-        print(f"Scanning {host} for open ports...")
-        # Append host IP address and open ports to results list
-        results.append(f"Host: {host}\nOpen Ports: {nm[host]['tcp'].keys()}\n\n")
+        print(f"Scanning {host} for vulnerabilities...")
+        # Run Nmap vulnerability scan on live host
+        output = os.popen(f"nmap -sV --script vuln {host}").read()
+        # Append vulnerability scan results to list
+        results.append(f"\nResults for {host}:\n{output}\n")
+    else:
+        print(f"No live hosts found on {subnet}")
 
 # Save results to a text file on the desktop
 if results:
-    with open(os.path.join(os.path.expanduser("~"), "Desktop", "network_scan_results.txt"), "w") as f:
+    with open(os.path.join(os.path.expanduser("~"), "Desktop", "scan_results.txt"), "w") as f:
         f.writelines(results)
-        print("Results saved to network_scan_results.txt on the desktop.")
+        print("Results saved to scan_results.txt on the desktop.")
 else:
-    print("No live hosts or open ports found on the network.")
+    print("No live hosts found on the network.")
