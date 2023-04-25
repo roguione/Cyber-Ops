bf1f82e7ccd7b421a51a51a55e58e114c77b2a0c
roguione <myjobcontacts@yahoo.com>
2023-04-25 13:32:38 -0500

    network map save txt added, and vulnerabilty stop can and pause after 5mins if nothing is found

--- ops-401-code-challenge/Nmapping.py
+++ ops-401-code-challenge/Nmapping.py
@@ -10,31 +10,38 @@
 
 import nmap
 import os
+import time
 
-# Get target IP address from user input
-subnet = input("Enter target IP address (with CIDR notation): ")
-
-# Get password from user input
-password = input("Enter password for sudo access: ")
-
-# Get TCP SYN scan time from user input
-tcp_syn_scan_time = input("Enter TCP SYN scan time (in seconds): ")
+# Define target subnet to scan
+subnet = input("Enter the IP address of the subnet you want to scan (e.g. 192.168.1.0/24): ")
+password = input("Enter the password to run the script as sudo: ")
 
 # Create Nmap scanner object
 nm = nmap.PortScanner()
 
-# Use Nmap to scan target subnet and identify live hosts
-nm.scan(hosts=subnet, arguments='-sn')
+# Use Nmap to scan target subnet and identify live hosts and open ports
+nm.scan(hosts=subnet, arguments='-p- -sS -T4')
 
-# Create a list to store vulnerability scan results
+# Create a list to store scan results
 results = []
+
+# Loop through identified live hosts and save IP and open ports to list
+for host in nm.all_hosts():
+    if nm[host]['status']['state'] == 'up':
+        open_ports = [str(port) for port in nm[host]['tcp'].keys() if nm[host]['tcp'][port]['state'] == 'open']
+        results.append(f"{host}\t{' '.join(open_ports)}\n")
+
+# Save results to a text file on the desktop
+with open(os.path.join(os.path.expanduser("~"), "Desktop", "scan_results.txt"), "w") as f:
+    f.writelines(results)
+    print("Results saved to scan_results.txt on the desktop.")
 
 # Loop through identified live hosts and run Nmap vulnerability scan
 for host in nm.all_hosts():
     if nm[host]['status']['state'] == 'up':
         print(f"Scanning {host} for vulnerabilities...")
         # Run Nmap vulnerability scan on live host with sudo
-        output = os.popen(f"echo {password} | sudo -S nmap -sS -sV -T2 -p- -R --randomize-hosts --min-rate=20 --max-retries 1 --max-scan-delay 0 --max-rtt-timeout 150ms --initial-rtt-timeout {tcp_syn_scan_time}s {host} --script vuln").read()
+        output = os.popen(f"echo {password} | sudo -S nmap -sS -sV -T2 -p- -R --randomize-hosts --min-rate=20 {host} --script vuln").read()
         # Append vulnerability scan results to list
         results.append(f"Results for {host}:\n{output}\n")
 
@@ -42,3 +49,7 @@ for host in nm.all_hosts():
 with open(os.path.join(os.path.expanduser("~"), "Desktop", "vulnerability_scan_results.txt"), "w") as f:
     f.writelines(results)
     print("Results saved to vulnerability_scan_results.txt on the desktop.")
+
+# Print "No vulnerabilities found" after 5 minutes of scanning
+time.sleep(300)
+print("No vulnerabilities found.")
