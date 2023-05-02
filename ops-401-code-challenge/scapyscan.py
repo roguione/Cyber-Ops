bd7126ab68a459c8f635a6ba271098c875a1b703
roguione <myjobcontacts@yahoo.com>
2023-05-01 23:33:44 -0500

    rough 2

--- ops-401-code-challenge/scapyscan.py
+++ ops-401-code-challenge/scapyscan.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
-from scapy.all import *
+from scapy.all import
+import os
 
 # Ask for host IP and port range to scan
 host = input("Enter the host IP address to scan: ")
@@ -18,6 +19,10 @@ while not ports:
         print("Invalid port input. Please try again.")
 
 # Loop through the port range and test each port
+open_ports = []
+closed_ports = []
+filtered_ports = []
+
 for port in ports:
     # Construct the packet
     packet = IP(dst=host)/TCP(dport=port, flags="S")
@@ -28,12 +33,35 @@ for port in ports:
         if response[TCP].flags == "SA":
             # Port is open
             print(f"Port {port} is open.")
+            open_ports.append(port)
             # Send a RST packet to close the connection gracefully
             rst_packet = IP(dst=host)/TCP(dport=port, flags="R")
             send(rst_packet, verbose=0)
         elif response[TCP].flags == "RA":
             # Port is closed
             print(f"Port {port} is closed.")
+            closed_ports.append(port)
         else:
             # Port is filtered and silently dropped
             print(f"Port {port} is filtered and silently dropped.")
+            filtered_ports.append(port)
+
+# Show scan results
+print("Scan complete!")
+print(f"Open ports: {open_ports}")
+print(f"Closed ports: {closed_ports}")
+print(f"Filtered ports: {filtered_ports}")
+
+# Ask if user wants to save results to desktop
+save_results = input("Do you want to save the scan results to your desktop? (Y/N): ")
+if save_results.upper() == "Y":
+    desktop_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
+    filename = input("Enter a filename for the results (without the file extension): ")
+    filepath = os.path.join(desktop_path, f"{filename}.txt")
+    with open(filepath, "w") as f:
+        f.write(f"Open ports: {open_ports}\n")
+        f.write(f"Closed ports: {closed_ports}\n")
+        f.write(f"Filtered ports: {filtered_ports}\n")
+    print(f"Results saved to {filepath}")
+else:
+    print("Scan results not saved.")
